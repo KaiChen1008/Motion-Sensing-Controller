a86b21478d11433961a0013edb0d85cd12590340
KaiChen1008 <1038790@gmail.com>
2021-06-11 13:28:52 +0800

    add computer receiver fix

--- scripts/receiver.py
+++ scripts/receiver.py
@@ -6,6 +6,8 @@ import json
 # ip = '169.254.57.236'
 ip = '192.168.0.164'
 
+sens = 2
+
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
     client.subscribe("test")
@@ -18,24 +20,22 @@ def on_message(client, userdata, msg):
     # m = msg.payload
     data = json.loads(msg.payload.decode('utf-8'))
     print(data['x'])
-    if data['y'] <= -3:
+    if data['y'] <= -sens:
         # left
         key = 'a'
-    elif data['y'] >= 3:
+    elif data['y'] >= sens:
         # right
         key = 'd'
-    elif data['x'] <= -3:
+    elif data['x'] <= -sens:
         # up
         key = 'w'
-    elif data['x'] >= 3:
+    elif data['x'] >= sens:
         key = 's'
 
     if key != None:
         keyboard.press(key)
         keyboard.release(key)
         print(f'enter {key}')
-
-    
 
     pass
 
